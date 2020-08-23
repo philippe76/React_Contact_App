b82333973d985bd11c7efab3dd1da09a032958b2
PHILIPPE LANOUGADERE <56220680+philippe76@users.noreply.github.com>
2020-08-23 22:48:23 +0200

    improve with arrow function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,19 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
-function addContact(contact) {
-  return (
-    <Card
-      key={contact.id}
-      name={contact.name}
-      img={contact.imgURL}
-      phone={contact.phone}
-      email={contact.email}
-    />
-  );
-}
-
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-
-      {contacts.map(addContact)}
+      {contacts.map((contact) => (
+        <Card
+          key={contact.id}
+          name={contact.name}
+          img={contact.imgURL}
+          phone={contact.phone}
+          email={contact.email}
+        />
+      ))}
     </div>
   );
 }
